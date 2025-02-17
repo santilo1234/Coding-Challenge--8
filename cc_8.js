f4ca5029579b2fa7d699757cd10e1eb00b05bf3c
santiago lopez <slopezarias@usf.edu>
2025-02-16 22:35:35 -0500

    Task 2: Function Expression

--- cc_8.js
+++ cc_8.js
@@ -8,3 +8,15 @@ function calculateSalary(baseSalary, bonus, taxRate){
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
 
+
+//Task 2
+const calculateDiscount= function(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    console.log(`Final Price: $$(finalPrice.toFixed(2))`); //Calculates final price after discount
+
+
+}
+
+//Test Data
+calculateDiscount(100, 0.2); // Expected output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
